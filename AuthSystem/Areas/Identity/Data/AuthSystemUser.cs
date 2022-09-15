ea868c623e298776d22ddc6bf429e96b106ca704
DCF <DCF@DESKTOP-VHJ2T91>
2022-09-15 11:20:46 +0800

    Add project files.

---
+++ AuthSystem/Areas/Identity/Data/AuthSystemUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace AuthSystem.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AuthSystemUser class
+public class AuthSystemUser : IdentityUser
+{
+    [ProtectedPersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [ProtectedPersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
+}
+
